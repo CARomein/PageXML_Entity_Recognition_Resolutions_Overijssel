948fb40cd0ae19025782657692b966430eb7ee7f
CA Romein <56157963+CARomein@users.noreply.github.com>
2025-11-04 09:55:41 +0100

    Delete run_dat_tagger.bat

--- run_dat_tagger.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================
-echo   Tagging DATES (DAT)
-echo ========================================
-call venv_dat\Scripts\activate
-python tag_entities_direct.py %1 %2 DAT
-call deactivate
-pause
